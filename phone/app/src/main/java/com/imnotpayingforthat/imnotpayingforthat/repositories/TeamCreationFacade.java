38c9dcbb230689e5aa91165888e6da54c36007a7
Unknown <Andersknudsen93@gmail.com>
2018-04-14 22:21:45 +0200

    FcmToken

--- phone/app/src/main/java/com/imnotpayingforthat/imnotpayingforthat/repositories/TeamCreationFacade.java
+++ phone/app/src/main/java/com/imnotpayingforthat/imnotpayingforthat/repositories/TeamCreationFacade.java
@@ -5,18 +5,17 @@ import android.util.Log;
 import com.google.firebase.auth.FirebaseAuth;
 import com.imnotpayingforthat.imnotpayingforthat.callbacks.TeamResponseHandler;
 import com.imnotpayingforthat.imnotpayingforthat.models.Team;
-import com.imnotpayingforthat.imnotpayingforthat.models.TeamResponse;
 import com.imnotpayingforthat.imnotpayingforthat.models.User;
 
 import java.util.HashMap;
 
 public class TeamCreationFacade {
     private final TeamRepository teamRepository;
-    private final UserRespository userRespository;
+    private final UserRepository userRepository;
 
     public TeamCreationFacade() {
         teamRepository = new TeamRepository();
-        userRespository = new UserRespository();
+        userRepository = new UserRepository();
     }
 
     public void createTeam(Team team, TeamResponseHandler responseHandler) {
@@ -25,13 +24,13 @@ public class TeamCreationFacade {
 
         teamRepository.createTeam(team, t -> {
             String teamId = t.getId();
-            userRespository.getUser(currentUserUid, u -> {
+            userRepository.getUser(currentUserUid, u -> {
                 User user = u.toObject(User.class);
                 if(user.getTeams() == null) {
                     user.setTeams(new HashMap<>());
                 }
                 user.addTeam(teamId);
-                userRespository.updateUser(currentUserUid, user, s -> {
+                userRepository.updateUser(currentUserUid, user, s -> {
                     responseHandler.createTeamResponse(true);
                 }, f -> {
                     Log.w("TEST", f);
